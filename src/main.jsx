fd20408b224002b856611f927df191e8f43dadc1
Ismail Hossen <144044351+error-ismail@users.noreply.github.com>
2026-09-24 01:55:16 +0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import OrderTracking from "./OrderTracking.jsx";
+
+createRoot(document.getElementById("root")).render(<OrderTracking />);
